a51e12591117f9b510c9c5bdba5df4360ff945fb
Petecks <j_peteck@hotmail.com>
2018-09-13 01:45:19 -0300

    RD stage

--- 8b10b.cpp
+++ 8b10b.cpp
@@ -65,42 +65,66 @@ for (size_t i=0; i<array3b.size(); ++i){
 
 //*****************************************************
 
+    int RD = 0;
+
   std::vector<int> mensagem(8,1);
 
   std::cout << "\n Mensagem Antes da codificacao:";
 
-  std::vector<string> mensagem10b; // miscela da mensagem a ser tratada
+  std::vector<string> mensagem10b(8); // miscela da mensagem a ser tratada
   for (std::vector<int>::iterator it = mensagem.begin(); it != mensagem.end(); ++it)
     std::cout << ' ' << *it;
 
-
+    std::cout << '\n' << mensagem10b.size() << endl;
 
 
     // ------------------lida com 5b
-    for (int i = 0 ; i < 5 ; i++){
-        if (to_string(mensagem[0]) == array[i][0] && to_string(mensagem[1]) == array[i][1] && to_string(mensagem[2]) == array[i][2] && to_string(mensagem[3]) == array[i][3] && to_string(mensagem[4]) == array[i][4]){
-            mensagem10b.push_back(array[i][5]);
-            mensagem10b.push_back(array[i][6]);
-            mensagem10b.push_back(array[i][7]);
-            mensagem10b.push_back(array[i][8]);
-            mensagem10b.push_back(array[i][9]);
-            mensagem10b.push_back(array[i][10]);
+    for (int i = 0 ; i < 33 ; i++){
+        if (to_string(mensagem[3]) == array[i][0] && to_string(mensagem[4]) == array[i][1] && to_string(mensagem[5]) == array[i][2] && to_string(mensagem[6]) == array[i][3] && to_string(mensagem[7]) == array[i][4]){
+            if (RD < 0){
+                mensagem10b.push_back(to_string(!stoi(array[i][5])));
+                mensagem10b.push_back(to_string(!stoi(array[i][6])));
+                mensagem10b.push_back(to_string(!stoi(array[i][7])));
+                mensagem10b.push_back(to_string(!stoi(array[i][8])));
+                mensagem10b.push_back(to_string(!stoi(array[i][9])));
+                mensagem10b.push_back(to_string(!stoi(array[i][10])));
+                RD++;
+            }else{
+                mensagem10b.push_back(array[i][5]);
+                mensagem10b.push_back(array[i][6]);
+                mensagem10b.push_back(array[i][7]);
+                mensagem10b.push_back(array[i][8]);
+                mensagem10b.push_back(array[i][9]);
+                mensagem10b.push_back(array[i][10]);
+                if ( stoi(array[i][5]) + stoi(array[i][6]) + stoi(array[i][7]) + stoi(array[i][8]) + stoi(array[i][9]) + stoi(array[i][10]) > 3) RD--;
+            }
         }
     }
 
+
     // ------------------lida com 3b
-    for (int i = 0 ; i < 3 ; i++){
+    for (int i = 0 ; i < 8 ; i++){
         if (to_string(mensagem[0]) == array3b[i][0] && to_string(mensagem[1]) == array3b[i][1] && to_string(mensagem[2]) == array3b[i][2] ){
-            mensagem10b.push_back(array3b[i][3]);
-            mensagem10b.push_back(array3b[i][4]);
-            mensagem10b.push_back(array3b[i][5]);
-            mensagem10b.push_back(array3b[i][6]);
+            if (RD < 0){
+                mensagem10b.push_back(to_string(!stoi(array3b[i][3]));
+                mensagem10b.push_back(to_string(!stoi(array3b[i][4]));
+                mensagem10b.push_back(to_string(!stoi(array3b[i][5]));
+                mensagem10b.push_back(to_string(!stoi(array3b[i][6]));
+                RD++;
+            }else{
+                mensagem10b.push_back(array3b[i][3]);
+                mensagem10b.push_back(array3b[i][4]);
+                mensagem10b.push_back(array3b[i][5]);
+                mensagem10b.push_back(array3b[i][6]);
+                if ( stoi(array[i][3]) + stoi(array[i][4]) + stoi(array[i][5]) + stoi(array[i][6]) > 0) RD--;
+            }
         }
     }
+
 
   std::cout << " \n Mensagem apos codificacao:";
   for (std::vector<string>::iterator it = mensagem10b.begin(); it != mensagem10b.end(); ++it)
     std::cout << ' ' << *it;
-
+    std::cout << '\n' << mensagem10b.size() << endl;
   return 0;
 }
